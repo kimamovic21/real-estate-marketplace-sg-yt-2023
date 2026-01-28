57225a0af2ee122e228e61a658820c96de013e51
Kerim Imamović <imamovic.kerim@gmail.com>
2026-01-28 16:26:12 +0100

    Create a test API route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
+import path from 'path';
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
+
 dotenv.config();
 
 mongoose
