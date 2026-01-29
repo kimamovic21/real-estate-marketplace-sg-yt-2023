7dd560b06020535c8b6d4fdbee5659e75553df67
Kerim Imamović <imamovic.kerim@gmail.com>
2026-01-29 14:19:28 +0100

    Complete sign up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
 
-export default function SignUp() {
+const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
+
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
@@ -24,13 +27,16 @@ export default function SignUp() {
         },
         body: JSON.stringify(formData),
       });
+
       const data = await res.json();
       console.log(data);
+
       if (data.success === false) {
         setLoading(false);
         setError(data.message);
         return;
       }
+
       setLoading(false);
       setError(null);
       navigate('/sign-in');
@@ -39,9 +45,11 @@ export default function SignUp() {
       setError(error.message);
     }
   };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
@@ -82,4 +90,6 @@ export default function SignUp() {
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
-}
+};
+
+export default SignUp;
