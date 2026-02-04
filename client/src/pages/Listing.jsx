7076c50894070435f57100c20ae529837efd5c42
Kerim Imamović <imamovic.kerim@gmail.com>
2026-02-04 14:00:27 +0100

    Complete show user listings functionality

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -91,7 +91,7 @@ export default function Listing() {
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
               {listing.name} - ${' '}
-              {listing.offer
+              {listing.offer && listing.discountPrice > 0
                 ? listing.discountPrice.toLocaleString('en-US')
                 : listing.regularPrice.toLocaleString('en-US')}
               {listing.type === 'rent' && ' / month'}
@@ -104,7 +104,7 @@ export default function Listing() {
               <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                 {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
               </p>
-              {listing.offer && (
+              {listing.offer && listing.discountPrice > 0 && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                   ${+listing.regularPrice - +listing.discountPrice} OFF
                 </p>
